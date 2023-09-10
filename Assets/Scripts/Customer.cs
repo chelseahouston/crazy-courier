dfda7744f19e6b4a7f369b9fedd3d4682221d82f
Chelsea Houston <chelseahouston29@gmail.com>
2023-09-10 10:07:51 +0100

    phone added on screen

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -23,12 +23,7 @@ public class Customer
     // Start is called before the first frame update
     void Start()
     {
-        
-        
-
     }
-
-
 
     public string GetName()
     {
--- Assets/Scripts/Job.cs
+++ Assets/Scripts/Job.cs
@@ -12,8 +12,8 @@ using Random = UnityEngine.Random;
 public class Job : MonoBehaviour
 {
 
-    public List<string> restaurants = new List<string>(); // list of all restaurants
-    public List<Customer> customers = new List<Customer>(); // list of all customers and addresses
+    public List<string> restaurants; // list of all restaurants
+    public List<Customer> customers;  // list of all customers and addresses
     public string jobRestaurant; // name of current job restaurant pickup location
     public Customer jobCustomer; // current job customer
     public int orderNo; // current order number
@@ -22,6 +22,7 @@ public class Job : MonoBehaviour
     public bool complete; // is current job complete?
     public GameObject restaurantGameObject; // where to pick up current order
     public GameObject customerHouseGameObject; // where to deliver current order
+    public bool jobOfferOnScreen = false; // if the potential job is shown on phone, false to start
 
     // Start is called before the first frame update
     void Start()
@@ -40,7 +41,8 @@ public class Job : MonoBehaviour
             "Baked",
             "Ramen Rhapsody"});
 
-        PopulateCustomers(customerData.GetCustomers());
+        
+        PopulateCustomers();
 
         // order no starts at 0 -- no orders yet!
         orderNo = 0;
@@ -60,15 +62,17 @@ public class Job : MonoBehaviour
         }
     }
 
-    public void PopulateCustomers(List<Customer> customers)
+    public void PopulateCustomers()
     {
-        this.customers = customers;
+        customerData = GetComponent<CustomerData>();
+        customers.AddRange(customerData.GetCustomers());
     }
 
     public void GenerateJob()
     {
         complete = false; // job not completed yet
         // coroutine of animation whilst "searching" for a job
+        jobOfferOnScreen = true;
 
         // get random restaurant
         int randomRest = Random.Range(0, restaurants.Count);
@@ -88,14 +92,14 @@ public class Job : MonoBehaviour
         // update TMPro UI to show current order on phone
         // remember to add # in front of orderNumber and $ in front of jobPay
         Debug.Log("Order #" + orderNo + " Pick up from: " + jobRestaurant + " Deliver to: " + jobCustomer.GetName() + " at " + jobCustomer.GetAddress() + " Payment: $" + jobPay);
-
+        
         AcceptJob(); // temp 
     }
 
     public void AcceptJob()
     {
         Debug.Log("Job Accepted :)");
-
+        jobOfferOnScreen = false;
         // set restaurant and customer home game objects for current job
         restaurantGameObject = GameObject.Find(jobRestaurant);
         restaurantGameObject.tag = "Current Restaurant";
@@ -107,6 +111,7 @@ public class Job : MonoBehaviour
 
     public void DeclineJob()
     {
+        jobOfferOnScreen = false;
         Debug.Log("Job Declined :(");
         GenerateJob(); // generate new job
     }
