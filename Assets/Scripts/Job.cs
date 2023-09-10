0953db8ebbc2a0c110823f69bfaf75aaab22daee
Chelsea Houston <chelseahouston29@gmail.com>
2023-09-10 11:40:24 +0100

    phone screen UI with job logic

--- Assets/Scripts/Job.cs
+++ Assets/Scripts/Job.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using TMPro;
 
 // @author: chelsea houston
 // @date-last-update-dd-mm-yy: 10-09-23
@@ -23,10 +24,18 @@ public class Job : MonoBehaviour
     public GameObject restaurantGameObject; // where to pick up current order
     public GameObject customerHouseGameObject; // where to deliver current order
     public bool jobOfferOnScreen = false; // if the potential job is shown on phone, false to start
+    public bool jobAccepted = false; // if a job has been accepted
+    public GameObject generatingJobScreen, jobScreen, acceptedJobScreen, declinedJobScreen; // phone screens
+    public TextMeshProUGUI  orderNoDisplay, restaurantDisplay, customerDisplay, payDisplay; // for job found
+    public TextMeshProUGUI acceptedOrderNo, acceptedRestaurant, acceptedCustomer, acceptedPay; // for job accepted
 
     // Start is called before the first frame update
     void Start()
     {
+        generatingJobScreen.SetActive(false);
+        jobScreen.SetActive(false);
+        acceptedJobScreen.SetActive(false);
+        declinedJobScreen.SetActive(false);
 
         // list of restaurants
         restaurants.AddRange(new List<string> {
@@ -41,8 +50,8 @@ public class Job : MonoBehaviour
             "Baked",
             "Ramen Rhapsody"});
 
-        
         PopulateCustomers();
+        DefaultPhoneText();
 
         // order no starts at 0 -- no orders yet!
         orderNo = 0;
@@ -60,6 +69,24 @@ public class Job : MonoBehaviour
         {
             GenerateJob();
         }
+        if (jobOfferOnScreen)
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                StartCoroutine(DeclineJobCoroutine());
+            }
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                AcceptJob();
+            }
+        }
+        if (jobAccepted)
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                DeclineJobCoroutine();
+            }
+        }
     }
 
     public void PopulateCustomers()
@@ -68,11 +95,23 @@ public class Job : MonoBehaviour
         customers.AddRange(customerData.GetCustomers());
     }
 
-    public void GenerateJob()
+    public IEnumerator GeneratingJobCoroutine()
     {
         complete = false; // job not completed yet
-        // coroutine of animation whilst "searching" for a job
+        jobAccepted = false; // job not accepted yet
+        generatingJobScreen.SetActive(true);
+        yield return new WaitForSeconds(Random.Range(1, 5));
+        generatingJobScreen.SetActive(false);
+        jobScreen.SetActive(true);
         jobOfferOnScreen = true;
+
+    }
+    public void GenerateJob()
+    {
+        acceptedJobScreen.SetActive(false);
+        declinedJobScreen.SetActive(false);
+        // coroutine of animation whilst "searching" for a job
+        StartCoroutine(GeneratingJobCoroutine());
 
         // get random restaurant
         int randomRest = Random.Range(0, restaurants.Count);
@@ -90,16 +129,29 @@ public class Job : MonoBehaviour
         jobPay = pay.ToString("#.##");
 
         // update TMPro UI to show current order on phone
-        // remember to add # in front of orderNumber and $ in front of jobPay
+        orderNoDisplay.text += orderNo;
+        restaurantDisplay.text += jobRestaurant;
+        customerDisplay.text += jobCustomer.GetName() + "<br>" + jobCustomer.GetAddress();
+        payDisplay.text += jobPay;
+
         Debug.Log("Order #" + orderNo + " Pick up from: " + jobRestaurant + " Deliver to: " + jobCustomer.GetName() + " at " + jobCustomer.GetAddress() + " Payment: $" + jobPay);
-        
-        AcceptJob(); // temp 
+       
     }
 
     public void AcceptJob()
     {
+        jobAccepted = true;
         Debug.Log("Job Accepted :)");
         jobOfferOnScreen = false;
+        jobScreen.SetActive(false);
+        acceptedJobScreen.SetActive(true);
+
+        // update TMPro UI to show current order on phone
+        acceptedOrderNo.text += orderNo;
+        acceptedRestaurant.text += jobRestaurant;
+        acceptedCustomer.text += jobCustomer.GetName() + "<br>" + jobCustomer.GetAddress();
+        acceptedPay.text += jobPay;
+
         // set restaurant and customer home game objects for current job
         restaurantGameObject = GameObject.Find(jobRestaurant);
         restaurantGameObject.tag = "Current Restaurant";
@@ -109,18 +161,43 @@ public class Job : MonoBehaviour
         // change UI on phone to say Current Job and details
     }
 
-    public void DeclineJob()
+    public IEnumerator DeclineJobCoroutine()
     {
         jobOfferOnScreen = false;
+        acceptedJobScreen.SetActive(false);
+        declinedJobScreen.SetActive(true);
         Debug.Log("Job Declined :(");
+        restaurantGameObject = GameObject.Find(jobRestaurant);
+        restaurantGameObject.tag = "Untagged";
+        customerHouseGameObject = GameObject.Find(jobCustomer.address);
+        customerHouseGameObject.tag = "Untagged";
+        yield return new WaitForSeconds(1.5f);
+        DefaultPhoneText();
         GenerateJob(); // generate new job
     }
 
     public void CompleteJob()
     {
-        // UI update good work etc
+        // update TMPro text to default
+        DefaultPhoneText();
         // update money wallet
         // update jobs completed
         complete = true;
+
+
+
+    }
+
+    public void DefaultPhoneText()
+    {
+        // update TMPro text to default
+        orderNoDisplay.text = "Order #";
+        restaurantDisplay.text = "Pickup: <br>";
+        customerDisplay.text = "Delivery: <br>";
+        payDisplay.text = "$";
+        acceptedOrderNo.text = "Order #";
+        acceptedRestaurant.text = "Pickup: <br>";
+        acceptedCustomer.text = "Delivery: <br>";
+        acceptedPay.text = "$";
     }
 }
