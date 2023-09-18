486b8953da05a897026a92eb509a4eaa858f7e24
Chelsea Houston <chelseahouston29@gmail.com>
2023-09-18 14:03:26 +0100

    music added

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sound[] music, sfx;
+    public AudioSource musicSource, sfxSource;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Slider sfxSlider;
+    public static AudioManager Instance;
+
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        LoadVolumePrefs();
+        PlayMusic("MainMusic");
     }
+
+    public void PlayMusic(string name)
+    {
+        Sound sound = Array.Find(music, x => x.name == name);
+
+        if (sound != null)
+        {
+            musicSource.clip = sound.clip;
+            musicSource.Play();
+        }
+
+        else
+        {
+            Debug.Log(name + " sound not found!");
+        }
+    }
+
+
+    public void PlaySFX(string name)
+    {
+        Sound sound = Array.Find(sfx, x => x.name == name);
+
+        if (sound != null)
+        {
+            sfxSource.clip = sound.clip;
+            sfxSource.Play();
+        }
+
+        else
+        {
+            Debug.Log(name + " sfx not found!");
+        }
+    }
+
+    public void SetMusicVolume(float volume) 
+    { 
+        musicSource.volume = volume;
+        PlayerPrefs.SetFloat("MusicVolume", musicSource.volume);
+
+    }
+
+    public void SetSFXvolume(float volume)
+    {
+        sfxSource.volume = volume;
+        PlayerPrefs.SetFloat("SfxVolume", sfxSource.volume);
+    }
+
+    void LoadVolumePrefs()
+    {
+        sfxSlider.value = PlayerPrefs.GetFloat("SfxVolume");
+        volumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+    }
+
+
+
 }
--- Assets/Scripts/MainOptionsMenu.cs
+++ Assets/Scripts/MainOptionsMenu.cs
@@ -11,7 +11,7 @@ public class MainOptionsMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        optionsMenu.SetActive(false);
     }
 
     // Update is called once per frame
